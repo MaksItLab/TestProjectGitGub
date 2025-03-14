9bb0232230afb2d7a6bfd7545889f98ede8d77d7
MaksItLab <maksim.ardintsev@mail.ru>
2025-03-14 13:45:38 +0300

    добавил файл

---
+++ TestProjectGitGub/Maksim.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectGitGub
+{
+    internal class Maksim
+    {
+        //12344444
+        //12344444
+        //12344444
+        //12344444
+        //12344444
+    }
+}
